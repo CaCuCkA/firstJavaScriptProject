1b897ac2d14575b71c291f8bf577ca16a718a263
Nickolay Yakovkin <vanilka4358@gmail.com>
2022-03-31 11:44:19 +0300

    Modified script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use srict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?",
-    "");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
 
 
 const personalMovieDB = {
@@ -13,12 +13,51 @@ const personalMovieDB = {
 
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+    }
+    else{
+        i--;
+    }
+}
+
+if(personalMovieDB.count < 10){
+    console.log('You have watched a few films');
+
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('You are common viewer');
+
+}else if(personalMovieDB.count > 30){
+    console.log('You are cinephile');
+}
+else{
+    console.log('Error');
+}
+
+// let i = 0;
+
+// while(i < 2){
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("На сколько оцените его?", "");
+
+//     personalMovieDB.movies[a] = b;
+//     i++;
+// }
+
+// let j = 0;
+
+// do{
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("На сколько оцените его?", "");
+
+//     personalMovieDB.movies[a] = b;
+//     j++;
+// }while(j < 2);
+
 
 console.log(personalMovieDB);
